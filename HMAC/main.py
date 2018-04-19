7b50d04136d15d2a310a21ee0f4250e185f49caa
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-04-19 13:11:25 -0700

    Update main.py

--- HMAC/main.py
+++ HMAC/main.py
@@ -1,24 +1,30 @@
-from allEncryption import RSAEncrypt
+from allEncryption import encryptDirectory
+from allDecryption import decryptDirectory
 from RSAKeyGen import RSAKeyGen
-from allDecryption import RSADecrypt
+from keyPaths import keyPaths
 from fileManagement import saveFileAsJSON, saveFile
+import os
+
 def main():
     #-----------------------------String test-----------------------------
     #print('\n\nBEGIN MYENCRYPT AND MYDECRYPT STRING TEST')
     #string_to_enc = "This is the test string to test out the encryption and decryption $
     #print('string to encrypt: ' + string_to_enc)
     #string_enc = encrypt.encrypt(string_key, string_to_enc, string_IV)
+
 #    print('encrypted string: ' + string_enc.decode("utf-8"))
     #string_dec = decrypt.decrypt(string_enc, string_key, string_IV)
     #print('decrypted string: ' + string_dec.decode("ascii"))
     #print('END MYENCRYPT AND MYDECRYPT STRING TEST')
     #-----------------------------/String test----------------------------
 
-    RSAKeyGen('.ssh/rsaKey.pem', '.ssh/rsaKey.pem.pub')
-    RSAcipher, ciphertext, iv, tag, ext = RSAEncrypt('P51.jpg', '.ssh/rsaKey.pem.pub')
-    saveFileAsJSON('toDecrypt.json', ciphertext, iv, RSAcipher, tag,  ext)
+    RSAKeyGen(keyPaths.pathToPrivateKey, keyPaths.pathToPublicKey)
+    workingDirectory = os.getcwd()
+    filePathToDirectory = workingDirectory +"/TestDirectory"
 
-    plaintext, ext = RSADecrypt('toDecrypt.json', '.ssh/rsaKey.pem')
-    saveFile('P51decrypted', plaintext, ext)
+    encryptDirectory(filePathToDirectory)
+
+    decryptDirectory(filePathToDirectory)
+
 if __name__ == '__main__':
     main()
